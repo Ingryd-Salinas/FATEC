e9a988aaa2db26b496aac0c882b81bf07b1b85e2
Ingryd <business.ingryd@gmail.com>
2026-04-22 16:40:24 -0300

    att

---
+++ 4°Semestre/Programação Orientada a Objetos/Grafico/src/Cliente.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Ingryd Salinas
+ * @since 
+ */
+public class Cliente {
+    String nome;
+    String cpf;
+    String nascimento;
+    String estado;
+    String sexo;
+    String observações;
+    boolean esportes;
+    boolean videogame;
+    boolean leitura;
+    boolean dança;
+    boolean marcenaria;
+    
+    //Construtor vazio
+    public Cliente(){
+        
+    }
+
+    //Construtor do objeto Cliente em memória
+    public Cliente(String nome, String cpf, String nascimento, String estado, String sexo, String observações, boolean esportes, boolean videogame, boolean leitura, boolean dança, boolean marcenaria) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.nascimento = nascimento;
+        this.estado = estado;
+        this.sexo = sexo;
+        this.observações = observações;
+        this.esportes = esportes;
+        this.videogame = videogame;
+        this.leitura = leitura;
+        this.dança = dança;
+        this.marcenaria = marcenaria;
+    }
+    
+    
+    
+}
